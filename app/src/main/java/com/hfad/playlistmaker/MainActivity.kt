130c1ba64fe65dfe5f0da04dc3a6e60c0b0a117c
Polina Kovaleva <kovalevap2019@gmail,com>
2026-03-30 16:27:11 +0300

    Выполнено: - Главный экран с кнопками - Экран настроек - Экраны медиатеки и поиска - Переходы между экранами - Все строки, цвета и размеры в ресурсах

--- app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.hfad.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.button_search)
+        val mediaButton = findViewById<Button>(R.id.button_media)
+        val settingButton = findViewById<Button>(R.id.button_setting)
+
+        searchButton.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+        }
+
+        mediaButton.setOnClickListener {
+            val intent = Intent(this, MediaActivity::class.java)
+            startActivity(intent)
+        }
+
+        settingButton.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
