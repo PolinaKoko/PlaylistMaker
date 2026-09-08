c56640f25022f802d13b7339c1bc4c36405a4a9c
Polina Kovaleva <kovalevap2019@gmail,com>
2026-09-08 13:48:42 +0300

    Спринт 19 Исправила видимость нижней панели в плеере и добавила разделитель.

--- app/src/main/java/com/hfad/playlistmaker/main/ui/MainActivity.kt
+++ app/src/main/java/com/hfad/playlistmaker/main/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.hfad.playlistmaker.main.ui
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -22,5 +23,19 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
 
         binding.bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.audioPlayerFragment -> {
+                    binding.bottomNavigationView.visibility = View.GONE
+                    binding.bottomDivider.visibility = View.GONE
+                }
+
+                else -> {
+                    binding.bottomNavigationView.visibility = View.VISIBLE
+                    binding.bottomDivider.visibility = View.VISIBLE
+                }
+            }
+        }
     }
 }
